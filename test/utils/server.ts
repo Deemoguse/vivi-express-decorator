04a04cf3dd632a9c03d624b3530c14c01635a4f7
Ruslan  Radionov <cugune.tyan@gmail.com>
2023-02-03 23:08:05 +0300

    fix: fix test

--- test/utils/server.ts
+++ test/utils/server.ts
@@ -1,8 +1,12 @@
 import express              from 'express';
 import { AttachController } from '../../src';
 
+let port = 8079;
+
 export default function startHttpServer (controllers: any[])
 {
+	port++;
+
 	const app = express();
 
 	AttachController(app, controllers);
