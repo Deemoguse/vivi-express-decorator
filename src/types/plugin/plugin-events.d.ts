4ff349cfd28f54330105e08e2db7affd3a891514
Ruslan  Radionov <cugune.tyan@gmail.com>
2023-07-19 23:33:25 +0300

    fix: Fixing `Plugin` Types

--- src/types/plugin/plugin-events.d.ts
+++ src/types/plugin/plugin-events.d.ts
@@ -3,14 +3,14 @@ import { PluginEventMap, PluginEventNames } from "./plugin-events-map";
 /**
  * Event callback function.
  */
-export type PluginEventCallback<Params, Config> =
-	| ((params: Params, config: Config) => void)
+export type PluginEventCallback<Params, Config = unknown> =
+	| ((params: Params, pluginConfig: Config) => void)
 	;
 
 /**
  * Array of event callback functions.
  */
-export type PluginEventCallbackSet<T extends PluginEventNames, Config> =
+export type PluginEventCallbackSet<T extends PluginEventNames, Config = unknown> =
 	| Array<PluginEventCallback<PluginEventMap[T], Config>>
 	;
 
@@ -18,6 +18,6 @@ export type PluginEventCallbackSet<T extends PluginEventNames, Config> =
  * The type of object where the key is the name of the event,
  * and the value is an array of its callback functions.
  */
-export type PluginEventSet<Config> =
+export type PluginEventSet<Config = unknown> =
 	| { [K in PluginEventNames]?: PluginEventCallbackSet<K, Config> }
 	;
