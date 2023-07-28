806da2e1a37e2f4209b1420fb78abf7e98ad0882
Ruslan  Radionov <cugune.tyan@gmail.com>
2023-07-29 00:57:44 +0300

    feat: add unit test for `plugin` module

---
+++ test/cases/modules/plugin.test.ts
@@ -0,0 +1,25 @@
+import Express from 'express';
+import { $, hl, hld } from '../../utils';
+
+describe(hl('Testing Plugin class. Using <express|magenta-bold>, Plugin, Storage:'), () => {
+	const app = Express();
+	const plugin = new $.Plugin();
+	const storage = new $.Storage();
+
+	it(hld('Should correctly configure plugin.'), () => {
+		const config = { key: 'value' };
+		plugin.configurate(config);
+		expect(plugin.config).toEqual(config);
+	});
+
+	it(hld('should correctly call event listeners.'), () => {
+		const mockListener = jest.fn();
+		const eventName: keyof $.PluginEventMap = 'attach-controller:after';
+		const eventParams: $.PluginEventMap[typeof eventName] = { app, storage };
+
+		plugin.on(eventName, mockListener);
+		plugin.callEvent(eventName, { app, storage });
+
+		expect(mockListener).toBeCalledWith(eventParams, plugin.config);
+	});
+});
