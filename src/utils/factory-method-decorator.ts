e51b24e1ff0b4c0cff8c4f0e07b532590eb314a9
Your Name <you@example.com>
2024-01-02 23:25:49 +0300

    fix: fix type name in factory-method-decorator and remove extra condition

--- src/utils/factory-method-decorator.ts
+++ src/utils/factory-method-decorator.ts
@@ -1,18 +1,16 @@
 import { config } from '../modules/config';
 import { pluginEventCaller } from './plugin-event-caller';
-import type { Http } from '../types/common/common-http';
+import type { CommonHttp } from '../types/common/common-http';
 import type { StorageSetHttpMethodParams } from '../types/storage/storage-method-params';
 
 /**
  * Produce a decorator function with the specified HTTP method.
  * @param method - HTTP method.
+ * @throws {TypeError} The `path` is not a string.
  **/
-export function factoryMethodDecorator (method: Http) {
+export function factoryMethodDecorator (method: CommonHttp) {
 	return (path: string) => {
-		if (typeof path !== 'string') {
-			throw new TypeError ('Error: the `path` argument must be a string');
-		}
-		return (target: any, name: string, descriptor: PropertyDescriptor) => {
+		return (target: any, _name: string, descriptor: PropertyDescriptor) => {
 			const params: StorageSetHttpMethodParams = { controller: target.constructor, function: descriptor.value, path, method };
 			pluginEventCaller('set-http-method:before', { storage: config.storage, params });
 			config.storage.setHttpMethod(params);
