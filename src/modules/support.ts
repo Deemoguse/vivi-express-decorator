7424e9b6a8be77cb493af975a86e1ddc8edb824d
Your Name <you@example.com>
2024-01-14 23:07:32 +0300

    fix(modules): Fixing the operation of the Support module and determining the current (active) stage of decorators

--- src/modules/support.ts
+++ src/modules/support.ts
@@ -1,6 +1,7 @@
 import { config } from './config';
 import type { SupportStageProposal } from '../types/support/support-stage-proposal';
-import type { SupportUniversePropertiesConstructor } from '../types/support/support';
+import type { ClassDecoratorContext, ClassMethodDecoratorContext } from '../types/common/common-type-provider';
+import type { SupportProvideDecorator, SupportProvideParams, SupportUniversePropertiesConstructor } from '../types/support/support';
 
 /**
  * Providing compatibility support between `Stage 2` and `Stage 3` Proposals.
@@ -13,41 +14,33 @@ export class Support {
 	/**
 	 * A certain stage of the proposal.
 	 */
-	private static _stageProposal: SupportStageProposal = config.support;
-
-	// Define Stage Proposal:
-	static {
-		if (config.support !== 'auto') {
-			class foo { @this._defineStageProposal bar () {}}
-		}
-	}
+	public static stageProposal: SupportStageProposal = config.support;
 
 	// Decorator for check:
 	private static _defineStageProposal (
 		target: Function | object,
-		nameOrContext: string | DecoratorContext,
+		nameOrContext?: string | ClassDecoratorContext | ClassMethodDecoratorContext,
 		descriptor?: PropertyDescriptor,
 	): void {
 		// Is Stage 2 Proposal:
 		if (
-			typeof target === 'function' &&
+			typeof target === 'object' &&
 			typeof nameOrContext === 'string' &&
 			typeof descriptor?.value === 'function'
 		) {
-			Support._stageProposal = 2;
+			Support.stageProposal = 2;
 		}
 
 		// Is Stage 3 Proposal
-		if (
-			descriptor === undefined &&
+		else if (
 			typeof target === 'function' &&
 			typeof nameOrContext === 'object'
 		) {
-			Support._stageProposal = 3;
+			Support.stageProposal = 3;
 		}
 
 		// Throw Error:
-		throw Error([
+		else throw Error([
 			'Error: The current Decorator Stage Proposal could not be determined.',
 			'Please use the "config.support" object of this package to set the current Decorator Stage Proposal.',
 		].join('\n'));
@@ -56,27 +49,30 @@ export class Support {
 	/**
 	 * To ensure type safety and compatibility of different versions of decorators.
 	 * Returns a new function with unified types.
-	 *
+	 * - - - - - - -
 	 * @param decoratorStage2 - The decorator implementing the Stage 2 Proposal.
 	 * @param decoratorStage3 - The decorator implementing the Stage 3 Proposal.
 	 */
 	public static provide<
-		DecoratorStage2 extends (...args: any[]) => any,
-		DecoratorStage3 extends (...args: any[]) => any,
+		DecoratorStage2 extends SupportProvideDecorator,
+		DecoratorStage3 extends SupportProvideDecorator<ThisParameterType<DecoratorStage2>>,
 		_Arguments extends SupportUniversePropertiesConstructor<DecoratorStage2, DecoratorStage3>
 	> (
-		decoratorStage2: DecoratorStage2,
-		decoratorStage3: DecoratorStage3,
+		params: SupportProvideParams<DecoratorStage2, DecoratorStage3>,
 	) {
 		return function (
 			controllerOrMethod: _Arguments['controllerOrMethod'],
 			nameOrContext?: _Arguments['nameOrContext'],
 			descriptor?: _Arguments['descriptor'],
 		) {
-			return Support._stageProposal === 2
-				? decoratorStage2(controllerOrMethod, nameOrContext, descriptor)
-				: decoratorStage3(controllerOrMethod, nameOrContext)
-				;
+			if (Support.stageProposal === 'auto') {
+				Support._defineStageProposal(controllerOrMethod, nameOrContext, descriptor);
+			}
+			const result = Support.stageProposal === 2
+				? params.decoratorStage2.bind(params.bind)
+				: params.decoratorStage3.bind(params.bind);
+
+			return result(controllerOrMethod, nameOrContext, descriptor);
 		};
 	}
 }
